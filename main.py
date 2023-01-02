1e17a84424fa25900a2610fc3b673db762a3d703
sudhanshujain2490 <sudhanshujain2490@gmail.com>
2023-01-02 16:52:17 +0530

    Chartink to Telegram through TradingView SS Version - 1.1

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import os
 from datetime import datetime as dt
 from selenium import webdriver
 from time import sleep
+
+from selenium.webdriver import Keys
+from selenium.webdriver.common import keys
 from selenium.webdriver.common.action_chains import ActionChains
 
 import pandas as pd
 import pytz
 import requests
 from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
 
 log = logging.getLogger(__name__)
 # NOTE while creating date objects you can use zone info which is very helpful when you are running scripts on outside India location
@@ -30,12 +34,18 @@ def get_stocks():
         s.headers['x-csrf-token'] = csrf
 
         process_url = 'https://chartink.com/screener/process'
+
+        # payload = {
+        #     'scan_clause': '( {57960} ( ( {cash} ( latest close > 3 days ago max ( 20 , latest high ) and 1 day ago  close <= 4 days ago  max ( 20 , latest high ) and 4 days ago high < 3 days ago max ( 20 , latest high ) and latest volume > latest sma ( volume,20 ) and latest close >= monthly "(1 candle ago high + 1 candle ago low + 1 candle ago close / 3)" and latest close >= monthly "( (1 candle ago high + 1 candle ago low + 1 candle ago close / 3 ) * 2 - 1 candle ago low )" and 1 day ago close < monthly "( (1 candle ago high + 1 candle ago low + 1 candle ago close / 3 ) * 2 - 1 candle ago low )" ) ) ) ) '
+        # }
+
         payload = {
-            'scan_clause': '( {57960} ( ( {cash} ( latest close > 3 days ago max ( 20 , latest high ) and 1 day ago  close <= 4 days ago  max ( 20 , latest high ) and 4 days ago high < 3 days ago max ( 20 , latest high ) and latest volume > latest sma ( volume,20 ) and latest close >= monthly "(1 candle ago high + 1 candle ago low + 1 candle ago close / 3)" and latest close >= monthly "( (1 candle ago high + 1 candle ago low + 1 candle ago close / 3 ) * 2 - 1 candle ago low )" and 1 day ago close < monthly "( (1 candle ago high + 1 candle ago low + 1 candle ago close / 3 ) * 2 - 1 candle ago low )" ) ) ) ) '
+            'scan_clause': '( {33489} ( latest close > latest ema ( latest close , 11 ) and 1 day ago close < latest ema ( latest close , 11 ) and latest close > latest ema ( latest close , 2 ) and 1 day ago close < latest ema ( latest close , 2 ) and latest volume > latest ema ( latest volume , 20 ) ) ) '
         }
         # payload = {
         #     'scan_clause': '( {cash} ( latest close > 1 day ago max ( 60 , latest high ) and latest close > latest open and latest close > 1 day ago close and latest volume >= 1 day ago volume * 3 and 1 day ago ema ( latest volume , 50 ) < latest sma ( latest volume , 50 ) + ( latest sma ( latest volume , 50 ) * 0.5 ) and 1 day ago ema ( latest volume , 50 ) > latest sma ( latest volume , 50 ) ) ) '
         # }
+
         # payload = {
         #     # NOTE Vishal Mehta Mean Reversion Selling - Place Limit Order at 1% of Latest Close Price 3% SL and 6% Target Exit all positions at 3PM
         #     'scan_clause': '( {cash} ( latest close > latest sma( close, 200 ) and latest rsi( 2 ) > 50 and '\
@@ -77,24 +87,29 @@ if __name__ == "__main__":
 
         # Here Chrome will be used
         driver = webdriver.Firefox()
-        print('firefox')
+        print('firefox check')
 
         # create action chain object
         action = ActionChains(driver)
-        print('driver')
+        print('driver check')
 
         # Opening the website
         driver.get(url)
         print('Opening the website')
-
         sleep(5)
         driver.maximize_window()
+        sleep(2)
+        action.send_keys(Keys.ESCAPE).perform()
+        sleep(2)
+        action.key_down(Keys.SHIFT).send_keys('f').key_up(Keys.SHIFT).perform()
+        sleep(2)
         driver.save_screenshot(str(i) + '.png')
         driver.quit()
 
         bot = telepot.Bot(apiToken)
         print(bot.getMe())
-        bot.sendPhoto(chatID, open(str(i) + '.png', 'rb'))
+        # bot.sendPhoto(chatID, open(str(i) + '.png', 'rb'), url)
+        bot.sendDocument(chatID, open(str(i) + '.png', 'rb'), url)
         print('photo send....')
         img_name = str(i) + '.png'
 
--- temp.py
+++ temp.py
@@ -1,3 +1,23 @@
-import os
+# import webdriver
+from selenium import webdriver
 
-os.remove('RECLTD.png')
+# import Action chains 
+from selenium.webdriver.common.action_chains import ActionChains
+
+# create webdriver object
+driver = webdriver.Firefox()
+
+# get geeksforgeeks.org
+driver.get("https://www.geeksforgeeks.org/")
+
+# get element 
+element = driver.find_element_by_class("remove-anchor__decoration")
+
+# create action chain object
+action = ActionChains(driver)
+
+# click the item
+action.click(on_element = element)
+
+# perform the operation
+action.perform()
